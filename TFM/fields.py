dd7022abaf048bfe044f70b81b1364a8cba070f6
AozoraMatsuda <matsuda-aozora759@g.ecc.u-tokyo.ac.jp>
2021-06-26 14:01:45 +0900

    remove piv

--- TFM/fields.py
+++ TFM/fields.py
@@ -2,12 +2,9 @@ import logging
 from os import stat
 from types import DynamicClassAttribute
 
-import imagej
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from numpy.fft.pocketfft import fft
-from pandas.core.arrays.sparse import dtype
 from pykalman import KalmanFilter
 
 from TFM import Vectors
@@ -34,38 +31,6 @@ class DPF(Vectors):
             data, columns=["x", "y", "vx", "vy", "m"], dtype=np.float64
         ).dropna()
         return DPF(df).confirm()
-
-    @classmethod
-    def PIV(
-        cls,
-        path: str,
-        piv1: int = 64,
-        sw1: int = 128,
-        piv2: int = 32,
-        sw2: int = 64,
-        piv3: int = 16,
-        sw3: int = 32,
-        correlation: float = 0.60,
-        save_path: str = None,
-    ) -> list:
-        print("START LOADIND FIJI....")
-        ij = imagej.init("/Applications/Fiji.app")
-        print("DONE!")
-        cnt = 1
-        res = []
-        for path1, path2 in zip(path[:-1], path[1:]):
-            path = save_path + "/" + str(cnt) + ".txt"
-            ij.py.run_macro(f"""open({path1});""")
-            ij.py.run_macro(f"""open({path2});""")
-            ij.py.run_macro("""run("Images to Stack", "name=Stack title=[] use");""")
-            ij.py.run_macro(
-                """
-run("iterative PIV(Basic)...", f"{piv1}=32 {sw1}=64 {piv2}=16 {sw2}=32 {piv3}=8 {sw3}=16 {correlation}=0.60 what=[Accept this PIV and output] noise=0.20 threshold=5 c1=3 c2=1 save=[{save_path}]");
-"""
-            )
-            df = DPF.load_DPF(path)
-            res.append(df)
-        return res
 
     @classmethod
     def generate_fields(
